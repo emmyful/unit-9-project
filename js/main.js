4a41e5672c2442aecd115f7ea67287239f6f6ae5
emmyful <emmelyn.tan@gmail.com>
2018-06-21 20:13:28 -0600

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// initialize the configuration of map
+function initMap() {
+  // use JS's built-in Navigator to get user's lat/lng coordinates
+  navigator.geolocation.getCurrentPosition(function(position) {
+    // create an object to store lat/lng data
+    var userLocation = {
+      lat: 40.8054491, 
+      lng: -73.9654415
+    };
+        // create a new instance of a map
+    // configure map with options object
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: userLocation,
+      zoom: 10,
+      scrollwheel: false
+    });
+    
+     var marker = new google.maps.Marker({
+      position: userLocation,
+      map: map,
+      title: 'User Location'
+    });
+
+    
+  });
+}
+
+initMap();
